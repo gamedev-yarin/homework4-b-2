2d88f8673c5b1a5f36039a23e2c244c47c53ef78
yarin Shapira <yariniii19@gmail.com>
2020-11-17 16:51:01 +0200

    Initial commit

---
+++ Assets/Scripts/WallDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroyer : MonoBehaviour
+{ //this script allows you to set borders to the screen so that everything that passes through them is deleted except for one tag that you can select
+        [SerializeField] string NotTriggeringTag;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag != NotTriggeringTag) Destroy(other.gameObject) ;
+    }
+}
